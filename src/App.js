0cf67bc8a02e6ba911e3de5e78819b2f8c275e9a
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-10-06 21:31:14 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
-import Hotel from './pages/hotel/Hotel';
-import List from './pages/list/List';
-import Login from './pages/login/Login';
+// import Hotel from './pages/hotel/Hotel';
+// import List from './pages/list/List';
+// import Login from './pages/login/Login';
 
 function App() {
   return (
