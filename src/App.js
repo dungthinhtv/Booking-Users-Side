783550fd8d4779a36f8ebcc9adbcca010b11fca9
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-10-06 23:04:22 +0700

    2 files modified, 6 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
-// import Hotel from './pages/hotel/Hotel';
+import Hotel from './pages/hotel/Hotel';
 import List from './pages/list/List';
-// import Login from './pages/login/Login';
+import Login from './pages/login/Login';
 
 function App() {
   return (
@@ -11,8 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/hotels" element={<List />} />
-        {/* <Route path="/hotels/:id" element={<Hotel />} />
-        <Route path="/login" element={<Login />} /> */}
+        <Route path="/hotels/:id" element={<Hotel />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import useFetch from '../../hooks/useFetch';
 import './featuredProperties.css';
 import { apiUrl } from '../../constants';
+import { Link } from 'react-router-dom';
 
 const FeaturedProperties = () => {
   const { data, loading, error } = useFetch(
@@ -17,7 +18,9 @@ const FeaturedProperties = () => {
           {data.map((item) => (
             <div className="fpItem" key={item._id}>
               <img src={item.photos[0]} alt="" className="fpImg" />
-              <span className="fpName">{item.name}</span>
+              <Link to={{ pathname: `/hotels/${item._id}` }}>
+                <span className="fpName">{item.name}</span>
+              </Link>
               <span className="fpCity">{item.city}</span>
               <span className="fpPrice">
                 Starting from ${item.cheapestPrice}
